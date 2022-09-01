7fe46bb541473522f46e21ec4d125da68df8ef6b
Ordaz Romero Sergio <orcheko@gmail.com>
2022-09-01 01:04:58 -0500

    Proyecto de xilófono terminado

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+   
+    @IBAction func pianoButtonAction(_ sender: UIButton) {
+        sender.layer.opacity = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.layer.opacity = 1
+        }
+        playSound(file: sender.currentTitle!)
+    }
     
-
+    
+    func playSound(file: String) {
+        guard let url = Bundle.main.url(forResource: file, withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
